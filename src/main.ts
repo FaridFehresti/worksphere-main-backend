e1c9e9b1b0800a3724d288bb8e5d1b86c9daaf5d
FaridFehresti <115652523+FaridFehresti@users.noreply.github.com>
2025-11-27 20:47:43 +0330

    added /api  prefix

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-  origin: "*",
-  credentials: false,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-  allowedHeaders: "Content-Type, Authorization",
-});
+  app.setGlobalPrefix('api'); // <<< ADD THIS
 
+  app.enableCors({
+    origin: "*",
+    credentials: false,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
+  });
 
   await app.listen(process.env.PORT ?? 5000);
 }
